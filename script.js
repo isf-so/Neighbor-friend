248541b14d7de43f135eb76a2506c0d4c81e906b
s <glcsofiane40@gmail.com>
2024-03-28 11:13:46 +0100

    sa

--- script.js
+++ script.js
@@ -27,3 +27,59 @@ for (let i = 1; i <= 25; i++) {
     listepersonne.appendChild(personne);
 }
 
+
+
+
+
+let option1 = 0;
+let option2 = 0;
+
+const evenementHTML = `
+<ul id="listeEvenements" style="overflow-y: scroll; max-height: 200px;">
+    <li class="evenement">
+        <h3>Concert en plein air</h3>
+        <p>Date: 20 avril 2024</p>
+        <p>Heure: 18h00</p>
+        <p>Lieu: Parc Central</p>
+    </li>
+</ul>`;
+
+const creerHTML = `
+<form>
+    <label for='titre'>Titre de l'événement:</label><br>
+    <input type='text' id='titre' name='titre'><br><br>
+    <label for='date'>Date:</label><br>
+    <input type='date' id='date' name='date'><br><br>
+    <label for='heure'>Heure:</label><br>
+    <input type='time' id='heure' name='heure'><br><br>
+    <label for='lieu'>Lieu:</label><br>
+    <input type='text' id='lieu' name='lieu'><br><br>
+    <button type='submit'>Créer l'événement</button>
+</form>`;
+
+function optiontitre1() {
+    const evenementsContainer = document.querySelector(".evenements");
+    const nouvelEvenementContainer = document.querySelector("#nouvelEvenement");
+
+    if (option1 === 0) {
+        evenementsContainer.innerHTML = ""; // Vide la liste d'événements s'il était affiché précédemment
+        nouvelEvenementContainer.innerHTML = creerHTML;
+        option1 = 1;
+    } else {
+        nouvelEvenementContainer.innerHTML = ""; // Vide le formulaire s'il était affiché précédemment
+        option1 = 0;
+    }
+}
+
+function optiontitre2() {
+    const evenementsContainer = document.querySelector(".evenements");
+    const nouvelEvenementContainer = document.querySelector("#nouvelEvenement");
+
+    if (option2 === 0) {
+        evenementsContainer.innerHTML = evenementHTML;
+        option2 = 1;
+    } else {
+        evenementsContainer.innerHTML = ""; // Vide la liste d'événements s'il était affiché précédemment
+        option2 = 0;
+    }
+}
